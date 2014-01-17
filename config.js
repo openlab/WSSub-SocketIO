4f15e4295a5f6020d72cac46ca00c97c0a1493e4
Nik Garkusha <nik@openlab.ca>
2014-01-17 12:31:35 -0800

    changed config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+	//"sbConnectionString":"Endpoint=sb://fanometer.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RUK5f0rSFkgQ0o3M31Wddx92ybASX2PLvyMh9R7PTTE=",
+	"sbConnectionString":"Endpoint=sb://fanometer-test.servicebus.windows.net/;SharedAccessKeyName=test;SharedAccessKey=rq4EzPv8+26XfN6aVTp+4/dKhW6jdat2cDDQVwLdLNk=",
+	"sbTopic": "seahawks",
+	"sbSubscription": "WebSocket"
+};
