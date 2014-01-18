bf61ebcdc2d4fbe39b5957dff93f2024ffaafaf4
Nik Garkusha <nik@openlab.ca>
2014-01-17 17:17:07 -0800

    socket io implementation

--- app.js
+++ app.js
@@ -1,44 +1,93 @@
-var azure = require('azure');
-var program = require('commander');
-var config = require('./config');
-var socketio = require('socket.io');
-var WebSocketServer = require('ws').Server;
+//require('newrelic');
+var express = require('express'),
+  http = require('http'),
+  path = require('path'),
+  hogan = require('hogan-express'),
+  util = require('util'),
+  app = express(),
+  config = require('./config'),
+  server = require('http').createServer(app),
+  azure = require('azure'),
+  io = require('socket.io').listen(server);
 
-var wss = new WebSocketServer({port: 8080});
-var sbService = azure.createServiceBusService(config.sbConnectionString);
+var serviceBusService = azure.createServiceBusService(config.sbConnectionString);
 
-program
-    .version('0.0.1')
-    .parse(process.argv);
+var currentSockets = [];
 
-var loop = setInterval(function() {
-    sbService.receiveSubscriptionMessage(config.sbTopic, config.sbSubscription, function(error, receivedMessage) {
+app.configure(function() {
+  app.set('port', process.env.PORT || 80);
+  app.engine('mustache', hogan);
+  app.set('view engine', 'mustache');
+  app.set('layout', __dirname + '/views/layout');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
 
-        if(error) {
-            console.error(error);
-            return;
-        }
+app.configure('development', function() {
+  app.use(express.errorHandler());
+});
 
-        console.log('Message received -->');
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
 
-        var msg = receivedMessage;
-        //console.log(msg);
-        //var msgjson = JSON.parse(msg);
-        //var msgbody = msgjson.body;
-        msgbody = msg.body;
-        //console.log("CORY IS AWESOME !");
-        //console.log(msgbody);
+// Handle 'connection' events
+io.sockets.on('connection', function(socket) {
+  socket.emit('fromServer', {
+    message: 'Connected!  There are now ' + io.sockets.clients().length + ' clients connected.'
+  });
 
-        var copy = new Buffer(msgbody);
-        console.log(copy);
+  currentSockets.push(socket);
 
-        wss.on('connection', function(ws) {
-            console.log(' ');
-            console.log('sending message...');
-            ws.send(copy);
-            console.log('message sent');
-        });
-    
-
+  //setInterval(function() { sendSampleMessage(socket) }, 5000);
+  socket.on('message', function(data) {
+    socket.emit('message', {
+      message: 'I sent: ' + data.message
     });
-}, 5);
+    socket.broadcast.emit('message', {
+      message: data.message
+    });
+  });
+
+});
+
+server.listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+function sendSampleMessage(socket) {
+  var msg = sample.getSample();
+  socket.emit('fromServer', {
+    message: msg
+  });
+}
+
+
+function getMessage() {
+  if(currentSockets.length > 0) {
+    serviceBusService.receiveSubscriptionMessage(config.sbTopic, config.sbSubscription, function(error, receivedMessage) {
+      if(!error) {
+        for(var i = currentSockets.length - 1; i >= 0; i--) {
+          console.log("[" + i + "] writing " + receivedMessage.content );
+          currentSockets[i].emit('fromServer', {
+            //message: receivedMessage.customProperties
+            message: receivedMessage
+          });
+        };
+      } else {
+        console.log("Error recieving message");
+        console.log(error);
+      }
+
+      getMessage();
+    });
+  } else {
+    setTimeout(getMessage, 3000);
+  }
+}
+
+getMessage();
--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 ﻿module.exports = {
 	//"sbConnectionString":"Endpoint=sb://fanometer.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RUK5f0rSFkgQ0o3M31Wddx92ybASX2PLvyMh9R7PTTE=",
-	"sbConnectionString":"Endpoint=sb://fanometer-test.servicebus.windows.net/;SharedAccessKeyName=test;SharedAccessKey=rq4EzPv8+26XfN6aVTp+4/dKhW6jdat2cDDQVwLdLNk=",
-	"sbTopic": "seahawks",
-	"sbSubscription": "WebSocket"
+	sbConnectionString:"Endpoint=sb://fanometer-test.servicebus.windows.net/;SharedAccessKeyName=test;SharedAccessKey=rq4EzPv8+26XfN6aVTp+4/dKhW6jdat2cDDQVwLdLNk=",
+	sbTopic: "seahawks",
+	sbSubscription: "WebSocket",
+	newRelicKey: "0fe4d83d929efd1f14adb863aab668966c72882a",
 };
